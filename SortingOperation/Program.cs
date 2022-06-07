e5148754638c290531ee2cd9971d9d3d5bd4c8ea
FryannM <fryannm@gmail.com>
2022-06-07 03:07:47 -0400

    initial commit

---
+++ SortingOperation/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static List<string> sortingOperations(int N, List<string> aWords)
+    {
+        string temp = string.Empty;
+        List<string> result = new List<string>();
+        aWords = aWords.Take(N).ToList();
+        foreach (string word in aWords)
+        {
+            var orderedWord = word
+                .GroupBy(x => x)
+                .OrderByDescending(x => x.Count())
+                .ThenBy(x => x.Key)
+                .SelectMany(x => x).ToList();
+
+            foreach (var itemWord in orderedWord)
+            {
+                temp += itemWord;
+                Console.Write(itemWord + " ");
+            }
+
+            Console.WriteLine();
+            result.Add(temp);
+            temp = string.Empty;
+            
+        }
+        Console.Read();
+        return result;
+    }
+
+    public static void Main()
+    {
+
+        sortingOperations(17, new List<string>()
+        {
+            //2
+            "abaccadcc",
+            "xyzxy",
+            //10
+            "dulgvgzwqg",
+            "gxtjtmtywr",
+            "hnlnxiupgt",
+            "gzjotckivp",
+            "dpwwsdptae",
+            "pcscpilknb",
+            "btvyhhmflf",
+            "artrtnqxcr",
+            "nrtcmcoadn",
+            "fkdsgnekft",
+            //5
+            "wzenwebuau",
+            "vokfxzynwl",
+            "neldfeyrxk",
+            "wqadfiodgs",
+            "ykiuvzfcbc"
+        });
+
+    }
+}
---
+++ test/UnitTest.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace test
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void Test_Quantity_Two()
+        {
+            int quantity = 2;
+            List<string> expected = new List<string> { "ccccaaabd", "xxyyz" };
+            var actual = Program.sortingOperations(quantity, new List<string> { "abaccadcc", "xyzxy" });
+            
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Contains(expected[i], actual[i]);
+            }
+
+        }
+
+        [Fact]
+        public void Test_Quantity_Ten()
+        {
+            int quantity = 10;
+            List<string> expected = new List<string> {
+            "gggdlquvwz",
+            "tttgjmrwxy",
+            "nnghilptux",
+            "cgijkoptvz",
+            "ddppwwaest",
+            "ccppbiklns",
+            "ffhhblmtvy",
+            "rrrttacnqx",
+            "ccnnadmort",
+            "ffkkdegnst"
+            };
+
+            var actual = Program.sortingOperations(
+                quantity,
+                new List<string> {
+                    "dulgvgzwqg",
+                    "gxtjtmtywr",
+                    "hnlnxiupgt",
+                    "gzjotckivp",
+                    "dpwwsdptae",
+                    "pcscpilknb",
+                    "btvyhhmflf",
+                    "artrtnqxcr",
+                    "nrtcmcoadn",
+                    "fkdsgnekft"
+                });
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Contains(expected[i], actual[i]);
+            }
+
+        }
+
+        [Fact]
+        public void Test_Quantity_Five()
+        {
+            int quantity = 5;
+            List<string> expected = new List<string> {
+            "eeuuwwabnz",
+            "fklnovwxyz",
+            "eedfklnrxy",
+            "ddafgioqsw",
+            "ccbfikuvyz"
+            };
+            var actual = Program.sortingOperations(quantity, new List<string> {
+                "wzenwebuau",
+                "vokfxzynwl",
+                "neldfeyrxk",
+                "wqadfiodgs",
+                "ykiuvzfcbc"
+            });
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Contains(expected[i], actual[i]);
+            }
+
+        }
+    }
+    
+}
